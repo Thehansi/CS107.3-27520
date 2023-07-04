181133775fc2d1c3c85490378a5ccfb2d3878d34
Thehansi <134836021+Thehansi@users.noreply.github.com>
2023-07-04 20:40:41 +0530

    Add files via upload

---
+++ 04th July 2023/Practical-Q6/Practical-Q6/ArrayClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Q6
+{
+    internal class ArrayClass
+    {
+        public int[] CreateArray(int size)
+        {
+            int[] array = new int[size * 2];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter value {i + 1}:");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                array[i * 2] = userInput;
+                array[i * 2 + 1] = 0;
+            }
+            return array;
+        }
+    }
+}
+            
+

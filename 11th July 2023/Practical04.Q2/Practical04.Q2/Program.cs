f17c5edf0cfb63ae15edad4ea241403b1d6d0162
Thehansi <134836021+Thehansi@users.noreply.github.com>
2023-07-11 11:09:15 +0530

    Add files via upload

---
+++ 11th July 2023/Practical04.Q2/Practical04.Q2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical04.Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmaticCalcuator calculator = new ArithmaticCalcuator();
+
+            calculator.Number1 = 20;
+            calculator.Number2 = 6;
+
+            int sum = calculator.Add();
+            int difference = calculator.Subtract();
+            int product = calculator.Multiply();
+            double quotient = calculator.Divide();
+
+            Console.WriteLine("Sum is : " + sum);
+            Console.WriteLine("Difference is : " + difference);
+            Console.WriteLine("Product is : " + product);
+            Console.WriteLine("Quotient is : " + quotient);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
+    
+
